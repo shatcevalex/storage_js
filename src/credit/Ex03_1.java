c24d2d5b24638397be389f2028115a72f287d27e
Alex <134911756+shatcevalex@users.noreply.github.com>
2023-06-15 19:15:11 +0300

    Homework Formula fixed

--- src/credit/Ex03_1.java
+++ src/credit/Ex03_1.java
@@ -7,15 +7,15 @@ public class Ex03_1 {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Введите значение а =  ");
+            System.out.print("Введите значение а =  ");
             int a = scanner.nextInt();
-            System.out.println("Введите значение b =  ");
+            System.out.print("Введите значение b =  ");
             int b = scanner.nextInt();
-            System.out.println("Введите значение r =  ");
+            System.out.print("Введите значение r =  ");
             int r = scanner.nextInt();
 
-            int D = (r * r);
-            int X = (a + b);
+            int D = (r * r) + (r * r);
+            int X = (a * a) + (b * b);
             while (true) {
 
                 if (D >= X) {

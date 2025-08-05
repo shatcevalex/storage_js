280173db2fc0b046dec3d1f81df3ad6e4cd98002
SAN <shatcevalex@gmail.com>
2025-08-05 20:07:12 +0200

    training

---
+++ src/work/Data_types_variables_operators.java
@@ -0,0 +1,50 @@
+package work;
+
+public class Data_types_variables_operators {
+    public static void main(String[] args) {
+
+        int integerVar = 10;
+        double doubleVar = 20.5;
+        char charVar = 'A';
+        boolean booleanVar = true;
+
+        System.out.println(integerVar);
+        System.out.println(doubleVar);
+        System.out.println(charVar);
+        System.out.println(booleanVar);
+
+        String name = "Alex";
+        int age = 31;
+        double averageGrade = 8.7;
+
+        System.out.println("__________________________________");
+        System.out.println("Имя " + name);
+        System.out.println("Возраст " + age);
+        System.out.println("Средний балл " + averageGrade);
+
+
+        int a = 45;
+        int b = 54;
+
+        System.out.println("__________________________________");
+        System.out.println("Сложение " + (a+b));
+        System.out.println("Вычитание " + (b-a));
+        System.out.println("Умножение " + (a*b));
+        System.out.println("Деление " + (b/a));
+        System.out.println("Остаток от деления " + (b%a));
+        System.out.println("___________________________________");
+
+
+        boolean isAdult = true;
+        boolean hasLicense = false;
+
+        if (isAdult && hasLicense) {
+            System.out.println("Можно водить машину?");
+        }else{
+            System.out.println("Нельзя водить машину");
+
+        }
+
+
+    }
+}

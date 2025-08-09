c5888ac85e852523004260945434deb8534a9569
SAN <shatcevalex@gmail.com>
2025-08-09 20:44:11 +0200

    training

---
+++ src/work/Cycles.java
@@ -0,0 +1,17 @@
+package work;
+
+public class Cycles {
+    public static void main(String[] args) {
+        // Программа которая выводит числа от 1 до 10 с помощью цикла for.
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+
+        }
+        // Dыводит числа от 10 до 1 с помощью цикла while.
+        int j = 10;
+        while (j  >= 0) {
+            System.out.println(j);
+            j--;
+        }
+    }
+}

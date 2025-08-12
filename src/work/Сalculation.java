9b5913d144264919d05249547fb5cea82dfa59ac
SAN <shatcevalex@gmail.com>
2025-08-12 19:04:42 +0200

    training

---
+++ src/work/Сalculation.java
@@ -0,0 +1,19 @@
+package work;
+
+import java.util.Scanner;
+
+public class Сalculation {
+    public static void main(String[] args) {
+
+        //Программа, которая:
+        // 1) Запрашивает у пользователя число n.
+        // 2) Вычисляет сумму всех чисел от 1 до n, которые делятся на 3 или на 5.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число ");
+        int n = scanner.nextInt();
+
+
+
+    }
+}

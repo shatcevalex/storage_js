fd760adfebf3882b80e70449c7230387b97daa5c
SAN <shatcevalex@gmail.com>
2025-08-16 18:20:40 +0200

    training

--- src/work/Сalculation.java
+++ src/work/Сalculation.java
@@ -13,12 +13,16 @@ public class Сalculation {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число ");
         int n = scanner.nextInt();
+        int sum = 0;
 
         for (int i = 1; i <= n ; i++) {
 
-            System.out.print(i+" ");
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i; // добавляем число к сумме
+
 
         }
+        System.out.println(sum);
 
 
 

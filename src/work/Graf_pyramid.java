62d677e1a19bc9c4bd96e8d052250446597e19b9
SAN <shatcevalex@gmail.com>
2025-08-18 23:30:20 +0200

    training

--- src/work/Graf_pyramid.java
+++ src/work/Graf_pyramid.java
@@ -1,7 +1,10 @@
 package work;
 
+import java.util.Scanner;
+
 public class Graf_pyramid {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
     }
 }

1fb9b3dabb51a8c00ea301c8278ed353c8dcccae
SAN <shatcevalex@gmail.com>
2025-08-19 22:02:43 +0200

    training

--- src/work/Graf_pyramid.java
+++ src/work/Graf_pyramid.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Graf_pyramid {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int n = scanner.nextInt();
 
     }
 }

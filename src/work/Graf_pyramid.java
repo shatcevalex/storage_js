e3503c6e3d805e109770d9107700452226fdc6ec
SAN <shatcevalex@gmail.com>
2025-08-26 21:10:37 +0200

    training

--- src/work/Graf_pyramid.java
+++ src/work/Graf_pyramid.java
@@ -7,7 +7,19 @@ public class Graf_pyramid {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
         int n = scanner.nextInt();
-        System.out.println("Вы выбрали " + n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i * 2 -1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println("*");
+
+        }
+
 
     }
 }

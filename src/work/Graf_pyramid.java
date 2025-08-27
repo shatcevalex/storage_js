f90c2154e55422ff733d8ecf27e50384536f7a49
SAN <shatcevalex@gmail.com>
2025-08-27 21:44:55 +0200

    training

--- src/work/Graf_pyramid.java
+++ src/work/Graf_pyramid.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Graf_pyramid {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число");
+        System.out.println(" Введите число ");
         int n = scanner.nextInt();
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j <= i * 2 -1; j++) {
+            for (int j = 0; j <= i * 2 -1; j++ ) {
                 System.out.print("*");
 
             }

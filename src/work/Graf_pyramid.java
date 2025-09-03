ba99ca2550df59b363a6df9e4f9a7de19f4395bd
SAN <shatcevalex@gmail.com>
2025-09-03 19:33:58 +0200

    training

--- src/work/Graf_pyramid.java
+++ src/work/Graf_pyramid.java
@@ -8,11 +8,11 @@ public class Graf_pyramid {
         System.out.println(" Введите число ");
         int n = scanner.nextInt();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0 ; i < n; i++) {
             for (int j = 0; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j <= i * 2 -1; j++ ) {
+            for (int j = 0 ; j <= i * 2 -1 ; j++ ) {
                 System.out.print("*");
 
             }

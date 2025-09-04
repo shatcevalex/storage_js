95caa0642a446b9b8053f262fe7a7419369c7434
SAN <shatcevalex@gmail.com>
2025-09-04 20:48:18 +0200

    training

--- src/work/Graf_pyramid.java
+++ src/work/Graf_pyramid.java
@@ -8,17 +8,27 @@ public class Graf_pyramid {
         System.out.println(" Введите число ");
         int n = scanner.nextInt();
 
-        for (int i = 0 ; i < n; i++) {
-            for (int j = 0; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0 ; j <= i * 2 -1 ; j++ ) {
-                System.out.print("*");
+        // Вывод только четных чисел
+       // for (int i = 0 ; i <= n; i++) {
+       //     if (i % 2 == 0){
+       //         System.out.print(i+" ");
+       //     }
+       //  }
 
-            }
-            System.out.println("*");
 
-        }
+        // С помощью while вывеk числа от n до 1 в обратном порядке.
+        // int i = 1;
+        // while (i <= n){
+        //    System.out.println(n + " ");
+        //    n--;
+        //   }
+
+      // С помощью do…while выведи числа от 1 до n.
+        int i = 1;
+        do {
+            System.out.println(i + " ");
+            i++;
+        } while (i <= n);
 
 
     }
